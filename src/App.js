704a702b7367d32fdd872509cd9571507812877d
Anna S <ann.starovoytova@gmail.com>
2021-10-30 21:32:39 +1300

    changed function to class in app.js, added functionality to link form, onInputChange, onButtonSubmit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-
+import React, { Component } from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import 'tachyons';
 
+const particalsOptions =  {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 700
+      }
+    },
+    move: {
+      speed: 2
+    }
+  }
+}
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-     {/*
-      <ImageLinkForm />
-      <FaceRecognition />*/}
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <Particles className='particles'
+                params={particalsOptions}
+              />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} 
+        />
+      {/*
+        <FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
